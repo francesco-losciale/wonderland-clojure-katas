8438998778cf7874978f5f42c829c476ef4342a6
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-12-20 16:46:58 +0000

    initial solution

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,18 @@
 (ns wonderland-number.finder)
 
+(defn has-same-digits? [n1 n2]
+  (= (set (str n1)) (set (str n2))))
+
+(defn is-magic? [n]
+  (and
+    (has-same-digits? n (* n 2))
+    (has-same-digits? n (* n 3))
+    (has-same-digits? n (* n 4))
+    (has-same-digits? n (* n 5))
+    (has-same-digits? n (* n 6))
+    )
+  )
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first (filter is-magic? (range 100000 999999)))
+  )
