71700aa85c4ac73486dfc3a3b5568bb117b4a648
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-12-20 13:36:31 +0000

    save initial version of encode

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -14,11 +14,23 @@
     s))
 
 (defn encode [keyword message]
+  (comment
+    (def alphabet "abcdefghijklmnopqrstuvwxyz")
+    (def m "m")
+    (def k "s")
+    (def rotations (dec (get-index m alphabet)))
+    (def index (get-index k alphabet))
+    (def keyword "s")
+    (def message "m"))
   (let [alphabet "abcdefghijklmnopqrstuvwxyz"
         keyword (resize-keyword keyword message)]
-
-    )
-  "encodeme")
+    (apply str
+           (for [[k m] [(map str keyword message)]]
+             (let [index (get-index k alphabet)
+                   rotations (dec (get-index m alphabet))]
+               (nth (rotate alphabet rotations) (dec index))))
+           )
+    ))
 
 (defn decode [keyword message]
   "decodeme")
