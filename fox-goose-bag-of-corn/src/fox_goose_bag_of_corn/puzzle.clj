c6d8092c60a5a3c6d84f1088deecb76f1cd92fe5
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-12-22 19:39:20 +0000

    initial commit

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -2,5 +2,34 @@
 
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
 
+; [:fox :goose :corn :you] [:boat] []
+; [:fox :goose ] [:boat] [:corn :you]
+; [:fox :goose :you] [:boat] [:corn ]
+; [:goose ] [:boat] [:corn :you :fox]
+; [:goose :you] [:boat] [:corn :fox]
+; [] [:boat] [:goose :you :corn :fox]
+
+(defn remaining-items [[item you] all-items]
+  (remove #(or (= % item) (= % you)) all-items))
+
+(defn danger? [items]
+  (or (= (set items) #{:goose :corn})
+      (= (set items) #{:fox :goose})))
+
 (defn river-crossing-plan []
-  start-pos)
+  (let [[[left _ right]] start-pos
+        items (filter #(not= % :you) left)
+        options (for [item items]
+                  (list item :you))]
+    (remove
+      #(danger?
+         (remaining-items % left))
+      options
+      )
+    ))
+
+; given left list
+; remove you and one more thing without leaving ...
+; copy on right list
+; add you to left list
+; repeat until left list empty
